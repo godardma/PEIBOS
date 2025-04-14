c918620ac7f083f2c81d2e91ecbd1d8e7bd267e8
godardma <mael.godard@ensta-bretagne.org>
2025-04-14 23:25:18 +0200

    new inflate

--- examples/Lorenz/Lorenz.cpp
+++ examples/Lorenz/Lorenz.cpp
@@ -10,7 +10,7 @@ int main()
   vectorField.setParameter("rho", 28.);
   vectorField.setParameter("beta", 8/3);
   
-  double tf=0.05;
+  double tf=0.15;
 
   VectorVar X(2);
   AnalyticFunction psi0 ({X},{1/sqrt(1+sqr(X[0])+sqr(X[1])),X[0]/sqrt(1+sqr(X[0])+sqr(X[1])),X[1]/sqrt(1+sqr(X[0])+sqr(X[1]))});
@@ -19,7 +19,7 @@ int main()
                                             {-2,1,3},
                                             {3,2,-1}});
 
-  double epsilon = 0.1;
+  double epsilon = 0.025;
   
   PEIBOS(vectorField, {0.}, psi0, generators, epsilon, "Atlas");
 
